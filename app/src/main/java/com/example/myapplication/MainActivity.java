62173a1ecc6d73f0fa7ba33f086c7d3f7238dd7d
William Lin <02william.lin@gulllakecs.org>
2021-02-08 13:01:50 -0500

    Changed Java Code and Added + and - Buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -25,10 +26,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+    /**
+     * This method is called when the + button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+    /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(77*2+1);
+        int numberOfCoffees = 2;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees*5);
     }
 
     /**
@@ -37,5 +54,12 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
